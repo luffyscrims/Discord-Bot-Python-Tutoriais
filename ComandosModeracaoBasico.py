95017f38ab99be99137e0a7f537a93b76f85828a
vagner2k18 <36799806+vagner2k18@users.noreply.github.com>
2018-04-07 09:41:28 -0300

    comando de ban,mute,desmultar

---
+++ ComandosModeracaoBasico.py
@@ -0,0 +1,62 @@
+#Importação basica
+import discord
+
+
+#Variavel para uso do discord
+client = discord.Client()
+
+#Evento de loggin do bot
+@client.event
+async def on_ready():
+    print("----Tutorial modecação-----")
+    print("Nome do bot{}".format(client.user.name))
+    print("Logado em {} servidores".format(len(client.servers)))
+    await client.change_presence(game=discord.Game(name="prefixo !",type= 1))
+    print("---Tutorial By vagner----")
+    print("---Lado Nego----")
+
+
+#Evento de messagem do bot
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith("!ban"):
+      try:
+        #Vai verificar se quem usou o comando tem permissão de adm   
+        if not message.author.server_permissions.administrator:
+            return await client.send_message(message.channel, '⚠️Permissão insuficiente')
+        author = message.author.mention
+        user = message.mentions[0]
+        await client.ban(user)
+        await client.send_message(message.channel,"Usuario: {} banido do server pelo Administrador: {}".format(user.mention,author))
+       #no caso do membro mencionado ser um adm vai enviar uma messagem
+      except  discord.errors.Forbidden:
+          return await client.send_message(message.channel, '⚠️ Nao posso banir o administrador :{}'.format(user.mention))
+
+
+
+    if message.content.lower().startswith("!mute"):
+        #vai verificar se quem usou o comando possui permissão de adm
+        if not message.author.server_permissions.administrator:
+            return await client.send_message(message.channel, '⚠️Permissão insuficiente')
+        author= message.author.mention
+        user = message.mentions[0]
+        """Lembrando que tem que ter o cargo mutado no seu server"""
+        cargo = discord.utils.get(message.author.server.roles, name='Mutado')
+        await client.add_roles(user, cargo)
+        await client.send_message(message.channel, 'O membro: {} foi mutado pelo Administrador: {}'.format(user.mention,author))
+
+    if message.content.lower().startswith("!desmultar"):
+        # vai verificar se quem usou o comando possui permissão de adm
+        if not message.author.server_permissions.administrator:
+            return await client.send_message(message.channel, '⚠️Permissão insuficiente')
+        author = message.author.mention
+        user = message.mentions[0]
+        """Lembrando que tem que ter o cargo mutado no seu server"""
+        cargo = discord.utils.get(message.author.server.roles, name='Mutado')
+        await client.remove_roles(user, cargo)
+        await client.send_message(message.channel, 'O membro: {} foi Desmultado pelo Administrador: {}'.format(user.mention, author))
+
+#CASO TENHA ALGUM ERRO NO CODIGO. ENTRE NO SERVIDOR LADO NEGRO IREMOS TE AJUDAR
+#AQUI VAI SEU TOKEN
+
+client.run('INSIRA SEU TOKEN AQUI')
