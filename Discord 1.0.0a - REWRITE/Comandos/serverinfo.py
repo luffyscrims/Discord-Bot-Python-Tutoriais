a2e2ba1320d88e70c07ddc1919b9bf83de74b00c
vagner2k18 <36799806+vagner2k18@users.noreply.github.com>
2018-08-19 12:58:38 -0300

    ♻️  Adicionado
    coloquei  o server info e a estrutura em on message

--- Discord 1.0.0a - REWRITE/Comandos/serverinfo.py
+++ Discord 1.0.0a - REWRITE/Comandos/serverinfo.py
@@ -1,1 +1,32 @@
-print("Em breve")
+import discord
+
+#declaração do client e o prefixo
+client = discord.Client()
+prefixo = "t!"
+
+#evento de login do bot
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(name='vagner tutorial', type=discord.ActivityType.playing))
+    print("----------ONLINE")
+    print(client.user.name)
+    print(client.user.id)
+    print(f"Em  {str(len(client.guilds))} Servidores")
+    print(f'Em contato com {str(len(set(client.get_all_members())))} usuarios')
+    print("---------")
+
+
+#evento de message do bot
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith(prefixo+"serverinfo"):
+        embedtest = discord.Embed(title='Server Info',color=0x83f68a,description=message.guild.name)
+        embedtest.add_field(name="Id",value=message.guild.id)
+        embedtest.add_field(name="Cargos", value=len(message.guild.roles))
+        embedtest.add_field(name="Membros", value=message.guild.member_count)
+        embedtest.add_field(name="Criado em", value=message.guild.created_at.strftime("%d %b %Y %H:%M"))
+        embedtest.add_field(name="Região", value=message.guild.region)
+        embedtest.set_thumbnail(url=message.author.avatar_url)
+        await message.channel.send(embed=embedtest)
+
+client.run("toke aqui")
--- Discord 1.0.0a - REWRITE/EstruturaBots/on_message.py
+++ Discord 1.0.0a - REWRITE/EstruturaBots/on_message.py
@@ -1,2 +1,32 @@
-def print(x):
- return x
+import discord
+
+#declaração do client e o prefixo
+client = discord.Client()
+prefixo = "t!"
+
+#evento de login do bot
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(name='vagner tutorial', type=discord.ActivityType.playing))
+    print("----------ONLINE")
+    print(client.user.name)
+    print(client.user.id)
+    print(f"Em  {str(len(client.guilds))} Servidores")
+    print(f'Em contato com {str(len(set(client.get_all_members())))} usuarios')
+    print("---------")
+
+
+#evento de message do bot
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith(prefixo+"embed"):
+        argumento = message.content[7:]
+        if not argumento:
+            await message.channel.send("**Insira um texto para colocar no embed**")
+            return
+        embedtest = discord.Embed(title='Embed Basico',color=0x83f68a,description=None)
+        embedtest.add_field(name="Campo 1",value=argumento)
+        embedtest.set_thumbnail(url=message.author.avatar_url)
+        await message.channel.send(embed=embedtest)
+
+client.run("Token aqui")
